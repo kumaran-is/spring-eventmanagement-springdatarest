c31ac4da30c3396064b1d8bdbf1d811eedbc7181
kumaran Iyyasamy srinivasan <kumaran.x.iyyasamy.srinivasan.-nd@disney.com>
2021-09-13 00:16:49 -0400

    implemented HAL browser

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # springboot-eventmanagement-springdatarest
 To Showcase CRUD operations using Spring Boot, Spring Data REST and MYSQL database
+
+# HAL Browser Support 
+Naviagte to the url `http://localhost:8080/api/v1/browser/index.html#/api/v1` in your browser
--- src/main/java/com/eventmanagement/demo/repository/EventRepository.java
+++ src/main/java/com/eventmanagement/demo/repository/EventRepository.java
@@ -1,15 +1,17 @@
 package com.eventmanagement.demo.repository;
 
-import java.util.List;
 import java.time.ZoneId;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.eventmanagement.demo.model.Event;
+import com.eventmanagement.demo.model.projections.PartialEventProjection;
 
+@RepositoryRestResource(excerptProjection=PartialEventProjection.class)
 public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
 	
 	// List<Event> findByName(@Param("name") String name);
